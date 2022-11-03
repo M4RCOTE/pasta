664dc8c09ce580e388238226057b755f04ad9a32
M4RCOTE <88394850+M4RCOTE@users.noreply.github.com>
2022-11-03 11:53:14 -0300

    Add files via upload

---
+++ atividade 03-11/somaDivisores.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	int s = 0;
+	
+	printf("soma divisores de: ");
+	scanf("%d", &a);
+
+	for(int i = 1; i<=a; i++){
+		if(a%i == 0){
+			s = s + i;
+		}
+	}
+
+	printf("%d\n", s);
+	return 0;
+}
